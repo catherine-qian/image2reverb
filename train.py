fb45ac51b4326570e372b271987d1167f50f68a2
xinyuanqian <catherine921229@hotmail.co.uk>
2022-03-12 16:27:29 +0800

    update eval

--- train.py
+++ train.py
@@ -7,18 +7,23 @@ from image2reverb.model import Image2Reverb
 from image2reverb.dataset import Image2ReverbDataset
 import time
 
+
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--loadckpt", type=str, default=None, help="load checkpoints to train")
     parser.add_argument("--n_gpus", type=int, default=1, help="How many GPUs to train with.")
     parser.add_argument("--checkpoints_dir", type=str, default="./checkpoints_image2reverb", help="Model location.")
     parser.add_argument("--batch_size", type=int, default=1, help="Batch size.")
-    parser.add_argument("--encoder_path", type=str, default="resnet50_places365.pth.tar", help="Path to pre-trained Encoder ResNet50 model.")
-    parser.add_argument("--depthmodel_path", type=str, default="mono_odom_640x192", help="Path to pre-trained depth (from monodepth2) encoder and decoder models.")
+    parser.add_argument("--encoder_path", type=str, default="resnet50_places365.pth.tar",
+                        help="Path to pre-trained Encoder ResNet50 model.")
+    parser.add_argument("--depthmodel_path", type=str, default="mono_odom_640x192",
+                        help="Path to pre-trained depth (from monodepth2) encoder and decoder models.")
     parser.add_argument("--dataset", type=str, default="./datasets/image2reverb", help="Dataset path.")
     parser.add_argument("--niter", type=int, default=100, help="Number of training iters.")
     parser.add_argument("--from_pretrained", type=str, default=None, help="Path to pretrained model.")
     parser.add_argument("--spectrogram", type=str, default="stft", help="Spectrogram type.")
-    parser.add_argument("--d_threshold", type=float, default=None, help="Value over which discriminator weights will be updated by optimizer.")
+    parser.add_argument("--d_threshold", type=float, default=None,
+                        help="Value over which discriminator weights will be updated by optimizer.")
     parser.add_argument("--version", type=str, default=None, help="Experiment version.")
     parser.add_argument("--no_depth", action="store_true", help="Don't apply the pre-trained depth model.")
     parser.add_argument("--no_places", action="store_true", help="Don't load Places365 weights for Encoder.")
@@ -30,7 +35,7 @@ def main():
 
     if args.no_places:
         args.encoder_path = None
-        
+
     if args.no_depth:
         args.depthmodel_path = None
 
@@ -43,16 +48,22 @@ def main():
         os.makedirs(folder)
 
     cuda = torch.cuda.is_available()
-    print('cuda: '+str(cuda))
+    print('cuda: ' + str(cuda))
     train_set = Image2ReverbDataset(args.dataset, "train", args.spectrogram)
     val_set = Image2ReverbDataset(args.dataset, "val", args.spectrogram)
 
-    train_dataset = torch.utils.data.DataLoader(train_set, shuffle=True, num_workers=args.num_worker, pin_memory=cuda, batch_size=args.batch_size)
-    val_dataset = torch.utils.data.DataLoader(val_set, num_workers=args.num_worker, batch_size=args.batch_size) # For now, to test
+    train_dataset = torch.utils.data.DataLoader(train_set, shuffle=True, num_workers=args.num_worker, pin_memory=cuda,
+                                                batch_size=args.batch_size)
+    val_dataset = torch.utils.data.DataLoader(val_set, num_workers=args.num_worker,
+                                              batch_size=args.batch_size)  # For now, to test
 
     # Main model
     model = Image2Reverb(args.encoder_path, args.depthmodel_path, d_threshold=args.d_threshold, t60p=not args.no_t60p)
-    
+    if args.loadckpt is not None or args.loadckpt == 'None':
+        print('load from pre-trained checkpoint')
+        m = torch.load(args.loadckpt, map_location=model.device)
+        model.load_state_dict(m["state_dict"])
+
     # Model training
     logger = loggers.TensorBoardLogger(
         args.checkpoints_dir,
@@ -67,7 +78,7 @@ def main():
         save_top_k=-1,
         verbose=True,
     )  # save the model periodically
-    
+
     trainer = Trainer(
         gpus=args.n_gpus if cuda else None,
         auto_select_gpus=True,
@@ -80,7 +91,7 @@ def main():
         callbacks=[checkpoint_callback],
         logger=logger
     )
-    
+
     trainer.fit(model, train_dataset, val_dataset)
 
 
